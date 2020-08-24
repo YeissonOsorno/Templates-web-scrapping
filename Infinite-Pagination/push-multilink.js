c9960fed8ed2d7a92ab6ef6c32adfd38a9e35600
YeissonOsorno <yeisson403@gmail.com>
2020-08-24 17:46:58 -0500

    push link

---
+++ Infinite-Pagination/push-multilink.js
@@ -0,0 +1,60 @@
+(function () {
+    var out = {};
+
+    if (typeof pass_it == "undefined") pass_it = {};
+    if (typeof msg == "undefined") msg = function (x) { return x; };
+
+    if (!pass_it["urls"]) {
+        out["pass_it"] = {
+            // Esta variable se usa en el pagination (Cuando los jobs sean > 0 se debe seguir paginando, en caso contrario se debe ir al siguiente link)
+            "jobs": 0,
+            // Arreglo de URLs
+            "array_urls": [],
+            "currentUrl": 0
+        };
+      } else {
+        out["pass_it"] = pass_it;
+    }
+    var link = document.querySelectorAll('selector_list_a');
+      for(var i = 0; i<link.length;i++){
+        var element = link[i];
+      
+        var url = element.href;
+        out["pass_it"].array_urls.push(url);
+      }
+     window.location.href = out["pass_it"].array_urls[0];
+    
+    out["wait"] = true;
+    return out;
+})();
+
+
+
+/************************************PAGINATION********************* */
+
+(function() {
+    var out = {};
+    var next_page_selector = 'div.pagination > a.selected + a'; 
+    var clickable_elem = document.querySelector(next_page_selector);
+      out["pass_it"] = pass_it;
+      msg("\x1b[32m -----------PAGINATION--------------");   
+      if (!clickable_elem) {
+          // Pregunta si la siguiente url existe
+          out["pass_it"]["currentUrl"] += 1;
+          // Pregunta si la siguiente url existe
+          if (out["pass_it"]["currentUrl"] < out["pass_it"]["array_urls"].length) {
+              var url = out["pass_it"].array_urls[out["pass_it"]["currentUrl"]];
+              window.location.href = url;
+              out["has_next_page"] = true;
+          } else {
+              out["has_next_page"] = false;
+          }
+    } else {
+          clickable_elem.click();
+            out["has_next_page"] = true;
+    }
+      
+      out.waitFor = "selector_jobs";
+      return out;
+  })();
+  
